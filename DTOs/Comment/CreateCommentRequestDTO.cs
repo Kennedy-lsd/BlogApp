0fd24b9557369de2282cfcc05b6e124eaa6372b4
Kennedy <santes1263@gmail.com>
2024-10-12 19:11:23 +0200

    Update DTOs from validation

--- DTOs/Comment/CreateCommentRequestDTO.cs
+++ DTOs/Comment/CreateCommentRequestDTO.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.DTOs.Comment
 {
     public class CreateCommentRequestDTO
-    {
+
+    {   [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 charachters")]
+        [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 charachters")]
+        [MaxLength(100)]
         public string Content { get; set; } = string.Empty;
     }
 }
--- DTOs/Stock/UpdateStockRequestDTO.cs
+++ DTOs/Stock/UpdateStockRequestDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,30 @@ namespace api.DTOs.Stock
 {
     public class UpdateStockRequestDTO
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Symbol must be at least 3 charachters")]
+        [MaxLength(100)]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Company Name must be at least 3 charachters")]
+        [MaxLength(100)]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 100000)]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 100)]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Industry can't be over 20 characters")]
         public string Industry { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, 10000000)]
         public long MarketCap { get; set; }
     }
 }
